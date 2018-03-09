2161bff62f18af5b6e55712f4bf8a8e89361efcd
Pierre Staub <pierre.staub31@gmail.com>
2018-03-09 17:10:38 +0100

    Update hello_world.rb

--- hello_world.rb
+++ hello_world.rb
@@ -1,7 +1,12 @@
 class HelloWorld
-   def self.hello
+   def self.hello(name)
        #
        # YOUR CODE GOES HERE
        #
+      if name != nil
+         puts "Hello, #{name}!"
+      else
+         puts "Hello World!"
+      end
    end
 end 
